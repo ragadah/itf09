8d81fc3756f43c86615003a581bf3ccd2ee8e0bf
Raghad A.Hilles <ragadhilles@gmail.com>
2023-09-17 21:14:47 +0300

    Task03

---
+++ Task03.py
@@ -0,0 +1,21 @@
+num1 = int(input("Enter two numbers:"))
+num2 = int(input("Enter two numbers:"))
+
+def add(n1,n2):
+    return n1 + n2
+
+def sub(n1,n2):
+    return n1 - n2
+
+def mul(n1,n2):
+    return n1 * n2
+
+def div(n1,n2):
+    if n2 == 0:
+        return "Error,cannot divide by zero"
+    return n1/n2
+
+print(add(num1,num2),type(add(num1,num2)))
+print(div(num1,num2),type(div(num1,num2)))
+print(mul(num1,num2),type(mul(num1,num2)))
+print(sub(num1,num2),type(sub(num1,num2)))
