fe1aa7f72d1d3337ea08ca14fa06d712cd730913
Raghad A.Hilles <ragadhilles@gmail.com>
2023-09-17 21:20:58 +0300

    Task03

--- Task03.py
+++ Task03.py
@@ -15,6 +15,8 @@ def div(n1,n2):
         return "Error,cannot divide by zero"
     return n1/n2
 
+#I added the type just because I wannna make sure I understood and applied all the information in the session
+
 print(add(num1,num2),type(add(num1,num2)))
 print(div(num1,num2),type(div(num1,num2)))
 print(mul(num1,num2),type(mul(num1,num2)))
