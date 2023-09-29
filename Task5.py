78b67ff2f9896660fe722760975e1274ad0a5b11
Raghad A.Hilles <ragadhilles@gmail.com>
2023-09-29 10:31:55 +0300

    Task5

--- Task5.py
+++ Task5.py
@@ -1,3 +1,5 @@
+#Question 1
+print("This is question's 1 answer")
 dict1 = {"Name": "Raghad", "Age": 20}
 dict2 = {"City": "Gaza", "Gender": "Female"}
 
@@ -9,3 +11,30 @@ for key, value in dict2.items():
     concatenated_dict[key] = value
 
 print("Concatenated Dict 3:", concatenated_dict)
+
+print("---------------------------")
+print("This is question's 2 answer")
+#Question 2
+def rect_area(length, width):
+    if length <= 0 or width <= 0:
+        raise ValueError("length and width are not positive numbers")
+    return length * width
+
+
+def rect_perimeter(length, width):
+    if length <= 0 or width <= 0:
+        raise ValueError("length and width are not positive numbers")
+    return 2 * (length + width)
+
+
+try:
+    length = float(input("Enter the length of the rectangle: "))
+    width = float(input("Enter the width of the rectangle: "))
+
+    area = rect_area(length, width)
+    perimeter = rect_perimeter(length, width)
+
+    print(f"The area of the rectangle: {area}")
+    print(f"The perimeter of the rectangle: {perimeter}")
+except ValueError as e:
+    print(f"Error: {e}")
